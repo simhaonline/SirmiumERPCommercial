e69a42e00cb0ac602975b5a3434425252130e40d
Milan Risti <milan.risti@gmail.com>
2019-07-05 15:08:30 +0200

    User Index, Admin Index

---
+++ SirmiumCommercial/wwwroot/lib_local/script_local/User_Local.js
@@ -0,0 +1,9 @@
+﻿var videoImgA = document.getElementById("videoImgActivity");
+var videoImgPlayA = document.getElementById("videoImgPlayActivity");
+
+videoImgA.onmouseenter = function () {
+    videoImgPlayA.hidden = false;
+}
+videoImgA.onmouseleave = function () {
+    videoImgPlayA.hidden = true;
+}
