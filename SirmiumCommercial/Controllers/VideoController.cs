c45c3f8fd989e78e257ca52e506080362b49424e
Milan Risti <milan.risti@gmail.com>
2019-10-08 18:22:44 +0200

    VideoGallery

---
+++ SirmiumCommercial/Controllers/VideoController.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SirmiumCommercial.Models;
+using SirmiumCommercial.Models.ViewModels;
+
+namespace SirmiumCommercial.Controllers
+{
+    public class VideoController : Controller
+    {
+        private IAppDataRepository repository;
+        private UserManager<AppUser> userManager;
+
+        public VideoController(IAppDataRepository repo, UserManager<AppUser> userMgr)
+        {
+            repository = repo;
+            userManager = userMgr;
+        }
+
+        public async Task<ViewResult> MyGallery(string id)
+        {
+            ViewData["Id"] = id;
+            ViewData["Title"] = "My Gallery";
+
+            AppUser user = await userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                //top 10
+                List<VideoInfo> top10 = new List<VideoInfo>();
+                //course videos
+                List<VideoInfo> courseVideos = new List<VideoInfo>();
+                //presentation videos
+                List<VideoInfo> presentationVideos = new List<VideoInfo>();
+                //representation videos
+                List<VideoInfo> representationVideos = new List<VideoInfo>();
+
+                foreach (Video video in repository.Videos
+                    .Where(v => v.CreatedBy == user.Id && v.Status != "Private")
+                    .OrderByDescending(v => v.DateAdded))
+                {
+                    VideoInfo vI = new VideoInfo
+                    {
+                        Video = video,
+                        CreatedBy = user,
+                        Likes = repository.Likes
+                        .Where(l => l.For == "Video" && l.ForId == video.Id).Count(),
+                        Dislikes = repository.Dislikes
+                        .Where(d => d.For == "Video" && d.ForId == video.Id).Count(),
+                        Comments = repository.Comments
+                        .Where(c => c.For == "Video" && c.ForId == video.Id).Count()
+
+                    };
+
+                    top10.Add(vI);
+
+                    switch (video.For)
+                    {
+                        case "Course":
+                            courseVideos.Add(vI);
+                            break;
+                        case "Presentation":
+                            presentationVideos.Add(vI);
+                            break;
+                        case "Representation":
+                            representationVideos.Add(vI);
+                            break;
+                    }
+                }
+
+                //most popular
+                List<VideoInfo> mostPopular = new List<VideoInfo>();
+                foreach(Video video in repository.Videos
+                    .Where(v => v.CreatedBy == user.Id && v.Status != "Private")
+                    .OrderByDescending(v => v.Views).Take(16))
+                {
+                    VideoInfo vI = new VideoInfo
+                    {
+                        Video = video,
+                        CreatedBy = user,
+                        Likes = repository.Likes
+                        .Where(l => l.For == "Video" && l.ForId == video.Id).Count(),
+                        Dislikes = repository.Dislikes
+                        .Where(d => d.For == "Video" && d.ForId == video.Id).Count(),
+                        Comments = repository.Comments
+                        .Where(c => c.For == "Video" && c.ForId == video.Id).Count()
+
+                    };
+                    mostPopular.Add(vI);
+                }
+
+                //private videos
+                List<VideoInfo> privateVideos = new List<VideoInfo>();
+                foreach (Video video in repository.Videos
+                    .Where(v => v.CreatedBy == user.Id && v.Status == "Private")
+                    .OrderBy(v => v.DateAdded))
+                {
+                    VideoInfo vI = new VideoInfo
+                    {
+                        Video = video,
+                        CreatedBy = user
+
+                    };
+                    privateVideos.Add(vI);
+                }
+
+                return View("VideoGallery", new GalleryViewModel {
+                    Top10 = top10.AsQueryable()
+                        .OrderByDescending(v => v.Likes - v.Dislikes).Take(10),
+                    MostPopular = mostPopular.AsQueryable(),
+                    Private = privateVideos.AsQueryable(),
+                    CourseVideos = courseVideos.AsQueryable(),
+                    PresentationVideos = presentationVideos.AsQueryable(),
+                    RepresentationVideos = representationVideos.AsQueryable()
+                });
+            }
+            else
+            {
+                //error
+                return View("Error");
+            }
+        }
+
+        public async Task<ViewResult> VideoGallery(string id)
+        {
+            ViewData["Id"] = id;
+            ViewData["Title"] = "Video Gallery";
+
+            AppUser user = await userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                //top 10
+                List<VideoInfo> top10 = new List<VideoInfo>();
+                //course videos
+                List<VideoInfo> courseVideos = new List<VideoInfo>();
+                //presentation videos
+                List<VideoInfo> presentationVideos = new List<VideoInfo>();
+                //representation videos
+                List<VideoInfo> representationVideos = new List<VideoInfo>();
+
+                foreach (Video video in repository.Videos
+                    .Where(v => v.Status != "Private")
+                    .OrderByDescending(v => v.DateAdded))
+                {
+                    AppUser createdBy = userManager.Users
+                        .FirstOrDefault(u => u.Id == video.CreatedBy);
+
+                    if (createdBy.CompanyName == user.CompanyName
+                        || createdBy.CompanyName == "")
+                    {
+                        VideoInfo vI = new VideoInfo
+                        {
+                            Video = video,
+                            CreatedBy = createdBy,
+                            Likes = repository.Likes
+                            .Where(l => l.For == "Video" && l.ForId == video.Id).Count(),
+                            Dislikes = repository.Dislikes
+                            .Where(d => d.For == "Video" && d.ForId == video.Id).Count(),
+                            Comments = repository.Comments
+                            .Where(c => c.For == "Video" && c.ForId == video.Id).Count()
+
+                        };
+
+                        top10.Add(vI);
+
+                        switch (video.For)
+                        {
+                            case "Course":
+                                courseVideos.Add(vI);
+                                break;
+                            case "Presentation":
+                                presentationVideos.Add(vI);
+                                break;
+                            case "Representation":
+                                representationVideos.Add(vI);
+                                break;
+                        }
+                    }
+                }
+
+                //most popular
+                List<VideoInfo> mostPopular = new List<VideoInfo>();
+                foreach (Video video in repository.Videos
+                    .Where(v => v.Status != "Private"))
+                {
+                    AppUser createdBy = userManager.Users
+                        .FirstOrDefault(u => u.Id == video.CreatedBy);
+
+                    if (createdBy.CompanyName == user.CompanyName
+                        || createdBy.CompanyName == "")
+                    {
+                        VideoInfo vI = new VideoInfo
+                        {
+                            Video = video,
+                            CreatedBy = createdBy,
+                            Likes = repository.Likes
+                        .Where(l => l.For == "Video" && l.ForId == video.Id).Count(),
+                            Dislikes = repository.Dislikes
+                        .Where(d => d.For == "Video" && d.ForId == video.Id).Count(),
+                            Comments = repository.Comments
+                        .Where(c => c.For == "Video" && c.ForId == video.Id).Count()
+
+                        };
+                        mostPopular.Add(vI);
+                    }
+                }
+
+                List<VideoInfo> privateVideos = new List<VideoInfo>();
+
+                return View(new GalleryViewModel
+                {
+                    Top10 = top10.AsQueryable()
+                        .OrderByDescending(v => v.Likes - v.Dislikes).Take(10),
+                    MostPopular = mostPopular.AsQueryable()
+                        .OrderByDescending(v => v.Video.Views).Take(16),
+                    Private = privateVideos.AsQueryable(),
+                    CourseVideos = courseVideos.AsQueryable(),
+                    PresentationVideos = presentationVideos.AsQueryable(),
+                    RepresentationVideos = representationVideos.AsQueryable()
+                });
+            }
+            else
+            {
+                //error
+                return View("Error");
+            }
+        }
+
+        public ViewResult NewVideo (string id)
+        {
+            ViewData["Id"] = id;
+
+            return View();
+        }
+    }
+}
--- SirmiumCommercial/Models/ViewModels/PlayerViewModel.cs
+++ SirmiumCommercial/Models/ViewModels/PlayerViewModel.cs
@@ -13,4 +13,82 @@ namespace SirmiumCommercial.Models.ViewModels
         public IQueryable<Comment> Comments { get; set; }
         public IQueryable<AppUser> Users { get; set; }
     }
+
+    public class VideoInfo
+    {
+        public Video Video { get; set; }
+        public AppUser CreatedBy { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public int Comments { get; set; }
+    }
+
+    public class GalleryViewModel
+    {
+        public IQueryable<VideoInfo> Top10 { get; set; }
+        public IQueryable<VideoInfo> MostPopular { get; set; }
+        public IQueryable<VideoInfo> Private { get; set; }
+        public IQueryable<VideoInfo> CourseVideos { get; set; }
+        public IQueryable<VideoInfo> PresentationVideos { get; set; }
+        public IQueryable<VideoInfo> RepresentationVideos { get; set; }
+    }
+
+    public class GalleryPartialViewModel
+    {
+        public IQueryable<VideoInfo> VideoInfo { get; set; }
+        public string DivId { get; set; }
+        public string BtnId { get; set; }
+        public string OnClickAction { get; set; }
+
+        public string VideoName(string name)
+        {
+            return (name.Length < 20) ?
+                $"<h3>{name}</h3>" :
+                "<h3 data-toggle='tooltip' data-placement='bottom'" +
+                $"title='{name}'>{name.Substring(0, 16)}...</h3>";
+        }
+
+        public string GalleryName(string name)
+        {
+            switch (name)
+            {
+                case "mostPopular":
+                    return "Most Popular";
+                case "top10":
+                    return "Top 10 Videos";
+                case "course":
+                    return "Course Videos";
+                case "presentation":
+                    return "Presentation Videos";
+                case "representation":
+                    return "Reresentation Videos";
+                default:
+                    return "Private Video";
+            }
+        }
+
+        public string onClickFunction (string onClickAction)
+        {
+            return $"collapseBtn.{onClickAction}()";
+        }
+
+        public string panelTopColor (string str)
+        {
+            switch (str)
+            {
+                case "mostPopular":
+                    return "hreddeep";
+                case "top10":
+                    return "hviolet";
+                case "course":
+                    return "horange";
+                case "presentation":
+                    return "hyellow";
+                case "representation":
+                    return "hgreen";
+                default:
+                    return "hnavyblue";
+            }
+        }
+    }
 }
